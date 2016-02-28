4ce53a37b63536e9a5357d8086b310eceb5a637d
Jaran Flaath <jaran.flaath@gmail.com>
2016-02-28 20:12:50 +0100

    Add integration test

---
+++ src/test/groovy/no/kodehuset/springboot/api/CarsIntegrationTest.groovy
@@ -0,0 +1,35 @@
+package no.kodehuset.springboot.api
+
+import groovy.json.JsonSlurper
+import no.kodehuset.springboot.Application
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.test.SpringApplicationConfiguration
+import org.springframework.boot.test.WebIntegrationTest
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+import org.springframework.web.client.RestTemplate
+
+import static org.junit.Assert.assertEquals
+
+@RunWith(SpringJUnit4ClassRunner)
+@SpringApplicationConfiguration(Application)
+@WebIntegrationTest(randomPort = true)
+class CarsIntegrationTest {
+
+    @Value('${local.server.port}')
+    int port
+
+    RestTemplate restTemplate = new RestTemplate()
+
+    @Test
+    void listShouldReturnAllCars() {
+
+        String response = restTemplate.getForEntity("http://localhost:" + port + "/cars", String).body
+
+        def json = new JsonSlurper().parseText(response)
+        assertEquals(3, json.size())
+    }
+
+
+}
