5176ea23884e3426ada04389b3f32d927cca80ea
Jaran Flaath <jaran.flaath@gmail.com>
2016-02-28 20:49:29 +0100

    Enable security

--- src/main/groovy/no/kodehuset/springboot/Application.groovy
+++ src/main/groovy/no/kodehuset/springboot/Application.groovy
@@ -2,8 +2,10 @@ package no.kodehuset.springboot
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 
 @SpringBootApplication
+@EnableWebSecurity
 class Application {
 
     public static void main(String[] args) {
---
+++ src/main/groovy/no/kodehuset/springboot/config/WebSecurityConfiguration.groovy
@@ -0,0 +1,18 @@
+package no.kodehuset.springboot.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+@Configuration
+class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+//                .antMatchers(HttpMethod.GET).permitAll()
+                .anyRequest().authenticated()
+
+    }
+}
