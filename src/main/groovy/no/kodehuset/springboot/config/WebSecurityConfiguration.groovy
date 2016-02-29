3d59215e0e0eda6ff09a75714e2c526cfd043061
Jaran Flaath <jaran.flaath@gmail.com>
2016-02-29 20:09:25 +0100

    re-add antMatchers pattern

--- src/main/groovy/no/kodehuset/springboot/config/WebSecurityConfiguration.groovy
+++ src/main/groovy/no/kodehuset/springboot/config/WebSecurityConfiguration.groovy
@@ -11,7 +11,7 @@ class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-//                .antMatchers(HttpMethod.GET).permitAll()
+                .antMatchers(HttpMethod.GET).permitAll()
                 .anyRequest().authenticated()
 
     }
