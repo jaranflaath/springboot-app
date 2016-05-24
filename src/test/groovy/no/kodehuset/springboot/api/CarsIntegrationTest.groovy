3e3c1090bdf1b9c93adb5924e97375c6c6bcc9bf
Jaran Flaath <jaran.flaath@gmail.com>
2016-05-24 20:06:41 +0200

    Update tests to 1.4.0 features

--- src/test/groovy/no/kodehuset/springboot/api/CarsIntegrationTest.groovy
+++ src/test/groovy/no/kodehuset/springboot/api/CarsIntegrationTest.groovy
@@ -1,29 +1,24 @@
 package no.kodehuset.springboot.api
 
 import groovy.json.JsonSlurper
-import no.kodehuset.springboot.Application
 import no.kodehuset.springboot.domain.Car
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.test.SpringApplicationConfiguration
-import org.springframework.boot.test.TestRestTemplate
-import org.springframework.boot.test.WebIntegrationTest
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
-import org.springframework.web.client.RestTemplate
+import org.springframework.boot.context.embedded.LocalServerPort
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.test.context.junit4.SpringRunner
 
 import static org.junit.Assert.assertEquals
-import static org.junit.Assert.assertNotNull
 
-@RunWith(SpringJUnit4ClassRunner)
-@SpringApplicationConfiguration(Application)
-@WebIntegrationTest(randomPort = true)
+@RunWith(SpringRunner)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CarsIntegrationTest {
 
-    @Value('${local.server.port}')
+    @LocalServerPort
     int port
 
-    RestTemplate restTemplate = new TestRestTemplate("jaran", "s3rp3nt")
+    TestRestTemplate restTemplate = new TestRestTemplate("jaran", "s3rp3nt")
 
 
     @Test
